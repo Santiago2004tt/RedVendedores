0e0a34037fc1cbfa628f2d7f48158a25625b5e2c
unknown <huendycaicedo@gmail.comg>
2022-10-08 21:12:55 -0500

    prueba de git

--- src/RedVendedores/test/Test.java
+++ src/RedVendedores/test/Test.java
@@ -3,6 +3,6 @@ package RedVendedores.test;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        System.out.println("miguel");
     }
 }
