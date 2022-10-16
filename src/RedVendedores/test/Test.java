4e5b1963e34123d5a16618b13b880aaa8a09afdd
Santiago2004tt <115126538+Santiago2004tt@users.noreply.github.com>
2022-10-15 20:57:09 -0500

    creacion de crud vendedores

--- src/RedVendedores/test/Test.java
+++ src/RedVendedores/test/Test.java
@@ -9,23 +9,23 @@ import junit.framework.TestCase;
  */
 public class Test extends TestCase {
 
-    static RedVendedores redVendedores = new RedVendedores("prueba");
+
 
     public static void main(String[] args) {
-        Vendedor vendedor3 = new Vendedor("jere", "Gamer", "11", "casa",null);
-        String mensajeResultado = redVendedores.crearVendedor(vendedor3);
-        System.out.println(mensajeResultado);
-        //System.out.println("miguel <3");
+
+
     }
 
     public static void testCrearVendedor(){
-        Vendedor vendedor3 = new Vendedor("jere", "Gamer", "11", "casa",null);
-        String mensajeResultado = redVendedores.crearVendedor(vendedor3);
+        RedVendedores redVendedores = new RedVendedores("prueba");
+        Vendedor vendedor = new Vendedor("jere", "Gamer", "11", "casa",null);
+        String mensajeResultado = redVendedores.crearVendedor(vendedor);
         assertEquals("el vendedor fue creado",mensajeResultado );
     }
 
 
     public static void testCrearVendedorRepetido(){
+        RedVendedores redVendedores = new RedVendedores("prueba");
         Vendedor vendedor = new Vendedor("jere", "Gamer", "11", "casa",null);
         Vendedor vendedor2 = new Vendedor("jere", "Gamer", "11", "casa",null);
         String mensajeResultado = redVendedores.crearVendedor(vendedor);
@@ -33,11 +33,20 @@ public class Test extends TestCase {
         assertEquals("El vendedor ya existe",mensajeResultado2 );
     }
 
-    public static void testVendedorExiste(){
+    public static void testBuscarVendedor(){
+        RedVendedores redVendedores = new RedVendedores("prueba");
         Vendedor vendedor = new Vendedor("jere", "Gamer", "11", "casa",null);
-        String mensajeResultado = redVendedores.crearVendedor(vendedor);
+        redVendedores.crearVendedor(vendedor);
         Vendedor vendedorEncontrado = redVendedores.buscarVendedor("11");
         assertEquals(vendedor,vendedorEncontrado );
+    }
+
+    public static void testEliminarVendedor(){
+        RedVendedores redVendedores = new RedVendedores("prueba");
+        Vendedor vendedor = new Vendedor("jere", "Gamer", "11", "casa",null);
+        redVendedores.crearVendedor(vendedor);
+        String mensajeObtenido = redVendedores.eliminarVendedor("11");
+        assertEquals("El vendedor fue eliminado", mensajeObtenido);
     }
 
 
